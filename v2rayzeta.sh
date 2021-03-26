78186be86cf475ef56b10f2a77d0c1d0c1d76062
zeta-sys <81003256+zeta-sys@users.noreply.github.com>
2021-03-26 11:08:28 -0300

    Update v2rayzeta.sh

--- v2rayzeta.sh
+++ v2rayzeta.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Author: Zeta
-git clone https://github.com/zeta-sys/instv2rayzeta.git && chmod 777 instv2rayzeta/instalador.sh && ./instv2rayzeta/instalador.sh && sudo rm -rf instv2rayzeta  
+git clone https://github.com/zeta-sys/instv2rayzeta.git
+chmod 777 instv2rayzeta/instalador.sh
+./instv2rayzeta/instalador.sh
+sudo rm -rf instv2rayzeta  
