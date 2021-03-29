6bc22121f88617e8bca4046e2260ebb0a39eb098
zeta-sys <81003256+zeta-sys@users.noreply.github.com>
2021-03-29 04:57:42 -0300

    Update zetav2ray2.sh

--- zetav2ray2.sh
+++ zetav2ray2.sh
@@ -2,21 +2,16 @@
 # Author: Zeta
 git clone https://github.com/zeta-sys/instv2rayzeta.git
 chmod 777 instv2rayzeta/instalador.sh
-echo $to_print
-	to_print='INSTALANDO  "V2RAY BY ZETA" ESPERE POR FAVOR......'
-	echo $to_print
-./instv2rayzeta/instalador.sh&> /dev/null && echo INSTALACION FINALIZADA CON EXITO || echo INSTALACION FINALIZADA CON EXITO
+echo -e "\e[1;41mINSTALANDO  "V2RAY BY ZETA" ESPERE POR FAVOR......\e[1;m"
+./instv2rayzeta/instalador.sh&> /dev/null && echo -e "\e[1;44mPANEL INSTALADO CON EXITO......\e[1;m"
 sudo rm -rf instv2rayzeta  
 while true; do
     read -p "¿DESEAS INSTALAR BADVPN (Necesario para juegos/videollamadas)? ESCRIBA YES o NO..."   yn
     case $yn in
         [Yy]* ) wget https://raw.githubusercontent.com/rockz5555/badvpn-installer/master/badvpn-installer.sh && bash badvpn-installer.sh; break;;
-        [Nn]* ) echo "DIGITE "V2RAY" PARA INCIAR - QUE LO DISFRUTE  :)";exit;;
-        * ) echo "Por favor responde \"yes\" para si o \"no\"";;
+        [Nn]* ) echo -e "\e[1;41mELIGIO NO INSTALAR BADVPN, SI DESEA INSTALAR, REINSTALE SCRIPT......\e[1;m";exit;;
+        * ) echo -e "\e[1;41mPOR FAVOR INGRESAR "YES" O "NO"......\e[1;m";;
     esac
 done
-
-to_print='INSTALACION FINALIZADA CON EXITO....   ;-)'
-	echo $to_print
-	to_print='DIGITE  "V2RAY"  PARA ENTRAR AL PANEL ADMINISTRADOR......'
-	echo $to_print
+echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m"
+echo -e "\e[1;44mINGRESE "V2RAY" PARA UTILIZAR EL PANEL ADMINISTRADOR......\e[1;m"
