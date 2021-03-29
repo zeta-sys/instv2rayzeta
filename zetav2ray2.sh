0e258ff3cc7d265aec88790392f23a4d4b6aa326
zeta-sys <81003256+zeta-sys@users.noreply.github.com>
2021-03-29 05:43:55 -0300

    Update zetav2ray2.sh

--- zetav2ray2.sh
+++ zetav2ray2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Author: Zeta
-git clone https://github.com/zeta-sys/instv2rayzeta.git
-chmod 777 instv2rayzeta/instalador.sh
+echo -e "\e[1;44mPARA INSTALAR DEBE SER USUARIO ROOT - VERIFICANDO......\e[1;m"                                                                            
+if [ "$EUID" = 0 ]                                                                   
+  then git clone https://github.com/zeta-sys/instv2rayzeta.git
+  chmod 777 instv2rayzeta/instalador.sh
 echo -e "\e[1;41mINSTALANDO  "V2RAY BY ZETA" ESPERE POR FAVOR......\e[1;m"
-./instv2rayzeta/instalador.sh
+./instv2rayzeta/instalador.sh&> /dev/null && echo -e "\e[1;44mPANEL INSTALADO CON EXITO......\e[1;m"
 sudo rm -rf instv2rayzeta  
 while true; do
     read -p "¿DESEAS INSTALAR BADVPN (Necesario para juegos/videollamadas)? ESCRIBA YES o NO..."   yn
@@ -15,3 +17,8 @@ while true; do
 done
 echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m"
 echo -e "\e[1;44mINGRESE "V2RAY" PARA UTILIZAR EL PANEL ADMINISTRADOR......\e[1;m"
+
+	                                                             
+  else echo -e "\e[1;41mINGRESE CON USUARIO ROOT......\e[1;m"                                                               
+  exit                                                                                 
+fi
