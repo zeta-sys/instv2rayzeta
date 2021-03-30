f63b6794d9ab2aa75ea128569f4bfd341e2755e8
zeta-sys <81003256+zeta-sys@users.noreply.github.com>
2021-03-30 01:35:48 -0300

    Update zetav2ray2.sh

--- zetav2ray2.sh
+++ zetav2ray2.sh
@@ -11,8 +11,8 @@ sudo rm -rf instv2rayzeta
 while true; do
     read -p "¿DESEAS INSTALAR BADVPN (Necesario para juegos/videollamadas)? ESCRIBA YES o NO..."   yn
     case $yn in
-        [Yy]* ) wget https://raw.githubusercontent.com/rockz5555/badvpn-installer/master/badvpn-installer.sh&> /dev/null && ./badvpn-installer.sh&> /dev/null; break;;
-        [Nn]* ) echo -e "\e[1;41mELIGIO NO INSTALAR BADVPN, SI DESEA INSTALAR, REINSTALE SCRIPT......\e[1;m" && echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m";exit;;
+        [Yy]* ) wget https://raw.githubusercontent.com/rockz5555/badvpn-installer/master/badvpn-installer.sh && ./badvpn-installer.sh&> /dev/null; break;;
+        [Nn]* ) echo -e "\e[1;41mELIGIO NO INSTALAR BADVPN, SI DESEA INSTALAR, REINSTALE SCRIPT......\e[1;m";exit;;
         * ) echo -e "\e[1;41mPOR FAVOR INGRESAR "YES" O "NO"......\e[1;m";;
     esac
 done
