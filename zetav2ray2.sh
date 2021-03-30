0ba321982171758d290f6e7ab276d2f60b7ee9c3
zeta-sys <81003256+zeta-sys@users.noreply.github.com>
2021-03-30 01:06:39 -0300

    Update zetav2ray2.sh

--- zetav2ray2.sh
+++ zetav2ray2.sh
@@ -10,13 +10,17 @@ sudo rm -rf instv2rayzeta
 while true; do
     read -p "¿DESEAS INSTALAR BADVPN (Necesario para juegos/videollamadas)? ESCRIBA YES o NO..."   yn
     case $yn in
-        [Yy]* ) wget https://raw.githubusercontent.com/rockz5555/badvpn-installer/master/badvpn-installer.sh && bash badvpn-installer.sh && echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m"; break;;
+        [Yy]* ) wget https://raw.githubusercontent.com/rockz5555/badvpn-installer/master/badvpn-installer.sh && bash badvpn-installer.sh; break;;
         [Nn]* ) echo -e "\e[1;41mELIGIO NO INSTALAR BADVPN, SI DESEA INSTALAR, REINSTALE SCRIPT......\e[1;m" && echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m";exit;;
         * ) echo -e "\e[1;41mPOR FAVOR INGRESAR "YES" O "NO"......\e[1;m";;
     esac
 done
-echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m"
-echo -e "\e[1;44mINGRESE "V2RAY" PARA UTILIZAR EL PANEL ADMINISTRADOR......\e[1;m"	                                                             
   else echo -e "\e[1;41mINGRESE CON USUARIO ROOT......\e[1;m"                                                               
 exit                                                                                 
 fi
+if [ "$EUID" = 0 ]                                                                   
+  then echo -e "\e[1;44mINSTALACION COMPLETADA......\e[1;m"
+echo -e "\e[1;44mINGRESE "V2RAY" PARA UTILIZAR EL PANEL ADMINISTRADOR......\e[1;m"
+  else
+    exit
+      fi
